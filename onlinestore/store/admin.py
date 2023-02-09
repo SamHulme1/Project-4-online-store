b5697cc9a9f2a991ec7573ac5e856908e3a4d1fc
SamHulme1 <samhulmephotography@gmail.com>
2023-02-09 13:17:25 +0000

    created model for create listing and applied migrations

---
+++ onlinestore/store/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import CreateListing
+
+admin.site.register(CreateListing)
+# Register your models here.
---
+++ onlinestore/store/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# create lsiting model
+# lists appropriate options for the user when they list an item for sale
+# listed items are sorted by the time they are listed newest first
+
+
+class CreateListing(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    description = models.TextField()
+    listedtime = models.DateField(auto_now_add=True)
+    sellerID = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='listed_seller_items')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.99)
+
+    class Condition(models.IntegerChoices):
+        NEW = 1, "New"
+        LNEW = 2, "Like New"
+        USED = 3, "Used"
+        NONF = 4, "None Functional"
+    condition = models.PositiveSmallIntegerField(
+        choices=Condition.choices, default=Condition.USED
+    )
+
+    class Catagory(models.IntegerChoices):
+        HOMEWARE = 1, "Homeware"
+        ELECTRONICS = 2, "Electronics"
+        ENTERTAINMENT = 3, "Entertainment"
+        CLOTHING = 4, "Clothing"
+        TOYS = 5, "Toys"
+        HEALTH = 6, "Health"
+
+    catagory = models.PositiveSmallIntegerField(
+        choices=Catagory.choices, default=Catagory.HOMEWARE
+    )
+
+    class Meta:
+        ordering = ['-listedtime']
+        indexes = [models.Index(fields=['-listedtime']),]
+
+    def __str__(self):
+        return self.title
