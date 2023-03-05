9ee2787b6ee98f0d5c900553755f63af94b7226b
SamHulme1 <samhulmephotography@gmail.com>
2023-03-05 15:28:29 +0000

    added registration for Createlisting model

--- onlinestore/store/admin.py
+++ onlinestore/store/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import CreateListing
 
-admin.site.register(CreateListing)
-# Register your models here.
+# listing order for listings on admin site
+# listings can be sorted by catagory, condition, price, sellerID and listedtime
+# listings are searchable by their title, descriptions and catagories
+
+
+@admin.register(CreateListing)
+class CreateListingOrder(admin.ModelAdmin):
+    list_display = ["title", "slug", "catagory",
+                    "description", "price", "condition",
+                    "sellerID", "listedtime", "stock"]
+    list_filter = ["catagory", "condition", "price", "sellerID", "listedtime",
+                   "stock"]
+    search_fields = ["title", "description", "catagory"]
+    raw_id_fields = ["sellerID"]
+    ordering = ["listedtime", "stock"]
