f3cb078b437cb068f676b0912f23ff80ab504192
SamHulme1 <samhulmephotography@gmail.com>
2023-03-14 18:41:47 +0000

    added urls for adding and sellers listing pages

--- onlinestore/store/urls.py
+++ onlinestore/store/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.all_listings, name='all_listings'), 
+    path('', views.all_listings, name='all_listings'),
+    path('seller/', views.get_sellers_listings, name='get_sellers_listings'),
     path('<int:id>/', views.listing_details, name='listing_details'),
+    path('add/', views.add_listing, name='add_listing'),
 ]
