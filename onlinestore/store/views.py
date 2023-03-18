43b5d3c3143ded49972d457e1443d8eb9ce5cfff
SamHulme1 <samhulmephotography@gmail.com>
2023-03-18 20:56:10 +0000

    added views for catagory and search results, added to view for creating a new listing and added filter to favourites view

--- onlinestore/store/views.py
+++ onlinestore/store/views.py
@@ -3,6 +3,24 @@ from django.contrib import messages
 from .models import Listing
 from django.contrib.auth.decorators import login_required
 from .forms import CreateNewListing
+from django.db.models import Q
+
+
+def catagory(request, cat):
+    listing_catagories = Listing.objects.filter(catagory=cat)
+    return render(request, "store/catagories.html", {
+        "cat": cat, "listing_catagories": listing_catagories})
+
+
+def search_results(request):
+    if request.method == "GET":
+        searched = request.GET["searched"]
+        results = Listing.objects.filter(Q(title__icontains=searched) |
+                                         Q(description__icontains=searched))
+        return render(request, 'store/search.html', {
+            "searched": searched, "results": results})
+    else:
+        return render(request, 'store/search.html', {})
 
 
 def get_sellers_listings(request):
@@ -26,17 +44,20 @@ def listing_details(request, id):
 
 @login_required
 def get_favourite(request):
-    listing_favourites = Listing.objects.filter(favourite=1)
+    listing_favourites = Listing.objects.filter(favourites=1)
     return render(request, 'store/listing_favourites.html', {
         'listing_favourites': listing_favourites})
 
 
 @login_required
 def add_listing(request):
+    user = request.user
     if request.method == "POST":
         form = CreateNewListing(request.POST, request.FILES)
         if form.is_valid():
-            listing = form.save()
+            new_listing = form.save()
+            new_listing.sellerID = user
+            new_listing.save()
             messages.success(request, 'listing created successfully')
         else:
             messages.error(request, 'listing not created'
