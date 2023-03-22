e14e4377d2bbf9bbde2da5d6710ca89278073ff2
SamHulme1 <samhulmephotography@gmail.com>
2023-03-22 15:19:18 +0000

    scripts for notification button and message display area

--- onlinestore/static/scripts/scripts.js
+++ onlinestore/static/scripts/scripts.js
@@ -7,12 +7,24 @@ window.onload=function(){
     conditional statement is used to eliminate typeError when the loaded document doesn't have a back button
     */
     let backButton = document.getElementById("back-button");
+    let messageButton = document.getElementById("message-button");
+    let messageDisplay = document.getElementById("message-area");
+    let notificationStatus = document.getElementById("notification");
     if (backButton == null){
     } else {
         backButton.addEventListener("click", function backToResults(){
             history.back()
         });
     }
+    messageButton.addEventListener("click", function viewMessages(){
+        notificationStatus.innerHTML = 
+        `<div id="message-button" class="btn">
+        <i class="fa-solid fa-bell notification-icon"></i>
+        </div>
+        <span id="notification-status-none" class="text-center">0</span>
+        `;
+        messageDisplay.removeAttribute("class");
+        });
 }
 
 
