278bdfde727939ac30a2149e5267957e69982bbf
SamHulme1 <samhulmephotography@gmail.com>
2023-04-04 18:00:18 +0000

    created urls for changing basket qunatity and adding to basket

--- onlinestore/basket/urls.py
+++ onlinestore/basket/urls.py
@@ -5,5 +5,7 @@ app_name = 'basket'
 
 urlpatterns = [
     path('', views.basket_view, name='basket_view'), 
-    path('add/<item_id>/', views.add_to_basket, name='add_to_basket')
+    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path(
+        'adjust/<item_id>/', views.change_basket_qty, name='change_basket_qty')
 ]
