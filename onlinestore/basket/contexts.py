b3f4eba21d480904809bfacac24b68381b95d2b5
SamHulme1 <samhulmephotography@gmail.com>
2023-04-04 18:03:24 +0000

    added code for getting the basket in local storage, and appending the objected addded to the basket to local storage and calculating total of objects in storage

--- onlinestore/basket/contexts.py
+++ onlinestore/basket/contexts.py
@@ -1,11 +1,26 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from store.models import Listing
 
 
 def basket_contents(request):
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        listed_product = get_object_or_404(
+            Listing, pk=item_id, stock=Listing.Stock.AVALIBLE)
+        total += quantity * listed_product.price
+        product_count += quantity
+
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'listed_product': listed_product
+        })
 
     delivery_price = total * Decimal(
         settings.STANDARD_DELEVERY_PERCENTAGE / 100)
