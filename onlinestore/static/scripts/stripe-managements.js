0b2951d0969c9c1e1e30c27a32255a4b45189320
SamHulme1 <samhulmephotography@gmail.com>
2023-04-10 18:23:16 +0000

    added scripts for passing data from the form to stripe and passing data from the save info section to the cache checkout view

--- onlinestore/static/scripts/stripe-managements.js
+++ onlinestore/static/scripts/stripe-managements.js
@@ -21,21 +21,57 @@ $( document ).ready(function() {
         ev.preventDefault();
         card.update({ 'disabled': true});
         $('#submit-btn').attr('disabled', true);
-        stripe.confirmCardPayment(clientSecret, {
-            payment_method: {
-                card: card,
-            }
-        }).then(function(result) {
-            if (result.error) {
-                let displayError = document.getElementById('card-errors');
-                displayError.textContent = result.error.message;
-                card.update({'disabled': false});
-                $('#submit-btn').attr('disabled', false);
-            } else {
-                if (result.paymentIntent.status === 'succeeded') {
-                    form.submit();
+        let saveInfo = Boolean($('#id-save-info').attr('checked'));
+        let csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+        let postData = {
+            'csrfmiddlewaretoken': csrfToken,
+            'client_secret': clientSecret,
+            'save_info': saveInfo,
+        };
+        let url = '/checkout/cache_checkout_session/';
+        $.post(url, postData).done(function() {
+            stripe.confirmCardPayment(clientSecret, {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: $.trim(form.first_name.value) + $.trim(form.last_name.value), 
+                        phone: $.trim(form.phone_number.value),
+                        email: $.trim(form.email.value),
+                        address: {
+                            line1: $.trim(form.address.value),
+                            city: $.trim(form.city.value), 
+                            country: $.trim(form.country.value), 
+                            state: $.trim(form.county.value)
+                        }
+                    }
+                },
+                shipping: {
+                    name: $.trim(form.first_name.value) + ' ' + $.trim(form.last_name.value), 
+                    phone: $.trim(form.phone_number.value),
+                    address: {
+                        line1: $.trim(form.address.value),
+                        city: $.trim(form.city.value), 
+                        country: $.trim(form.country.value), 
+                        state: $.trim(form.county.value),
+                        postal_code: $.trim(form.postcode.value)
+                    }
                 }
-            }
-        });
+            }).then(function(result) {
+                if (result.error) {
+                    let displayError = document.getElementById('card-errors');
+                    displayError.textContent = result.error.message;
+                    card.update({'disabled': false});
+                    $('#submit-btn').attr('disabled', false);
+                } else {
+                    if (result.paymentIntent.status === 'succeeded') {
+                        form.submit();
+                    } else {
+    
+                    }
+                }
+            });
+        }).fail(function () {
+            location.reload()
+        })
     });
 });
